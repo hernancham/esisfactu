65f26449e27ce6efc3dad3e994ff36df48e7202f
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-09-11 12:45:24 -0500

    Add authentication and authorization middleware

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { middleware } from "@/auth";
+
+import { publicRoutes, authRoutes, apiAuthRoute } from "./auth/routes";
+
+export default middleware((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  // console.log({ isLoggedIn, path: nextUrl.pathname });
+
+  // Permitir todas las rutas de API de autenticación
+  if (nextUrl.pathname.startsWith(apiAuthRoute)) {
+    return NextResponse.next();
+  }
+
+  // Permitir acceso a rutas públicas sin importar el estado de autenticación
+  if (publicRoutes.includes(nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  // Redirigir a /dashboard si el usuario está logueado y trata de acceder a rutas de autenticación
+  if (isLoggedIn && authRoutes.includes(nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/dashboard", nextUrl));
+  }
+
+  // Redirigir a /login si el usuario no está logueado y trata de acceder a una ruta protegida
+  if (
+    !isLoggedIn &&
+    !authRoutes.includes(nextUrl.pathname) &&
+    !publicRoutes.includes(nextUrl.pathname)
+  ) {
+    return NextResponse.redirect(new URL("/login", nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
